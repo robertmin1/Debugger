6dbdd20de09311c3e9a34f39832550bfe16acb90
robertmin1 <104002271+robertmin1@users.noreply.github.com>
2022-10-28 17:29:13 +0300

    Test Data Bash

--- testdata/test.bash
+++ testdata/test.bash
@@ -3,4 +3,8 @@
 set -euo pipefail
 shopt -s nullglob globstar
 
+git clone https://github.com/robertmin1/Debugger && cd Debugger
+export GOBIN="$PWD"
+go install WordGenerator.go
+
 timeout 10s go run main.go
