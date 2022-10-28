05ba611154f85e906654c49224e1d18409dffe1d
robertmin1 <104002271+robertmin1@users.noreply.github.com>
2022-10-28 17:04:08 +0300

    Test Data Bash

--- testdata/test.bash
+++ testdata/test.bash
@@ -5,5 +5,5 @@ shopt -s nullglob globstar
 
 git clone https://github.com/robertmin1/Debugger && cd Debugger
 export GOBIN="$PWD"
-
-timeout 10s go run WordGenerator.go
+go build WordGenerator.go
+timeout 10s go run main.go
