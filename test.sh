bc01250f03582642171db688ad801399cda525f9
robertmin1 <104002271+robertmin1@users.noreply.github.com>
2022-10-28 13:22:39 +0300

    Test Data Shell

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-git clone https://github.com/robertmin1/Debugger && cd "!$:t:r"
+git clone https://github.com/robertmin1/Debugger && cd Debugger
 export GOBIN="$PWD"
 go install WordGenerator.go
-go run main.go
+timeout 10s go run main.go
