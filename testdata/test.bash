7d409188654f9999fbf779d81592f02140b2d951
robertmin1 <104002271+robertmin1@users.noreply.github.com>
2022-10-28 15:50:12 +0300

    Test Data Bash

--- testdata/test.bash
+++ testdata/test.bash
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+set -euo pipefail
+shopt -s nullglob globstar
+
 git clone https://github.com/robertmin1/Debugger && cd Debugger
 export GOBIN="$PWD"
 go install WordGenerator.go
